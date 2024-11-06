297262b1fe5a6d89a2de3447d4314fa87726987c
Pau Azpeitia <pau.apa.azpeitia@gmail.com>
2024-11-06 17:48:45 +0100

    Hecho por ChatGpt

--- labs/04/mnist_competition.py
+++ labs/04/mnist_competition.py
@@ -9,70 +9,52 @@ import urllib.request
 
 import numpy as np
 import numpy.typing as npt
+from sklearn.neural_network import MLPClassifier
 
 parser = argparse.ArgumentParser()
-# These arguments will be set appropriately by ReCodEx, even if you change them.
 parser.add_argument("--predict", default=None, type=str, help="Path to the dataset to predict")
 parser.add_argument("--recodex", default=False, action="store_true", help="Running in ReCodEx")
 parser.add_argument("--seed", default=42, type=int, help="Random seed")
-# For these and any other arguments you add, ReCodEx will keep your default value.
 parser.add_argument("--model_path", default="mnist_competition.model", type=str, help="Model path")
 
-
 class Dataset:
-    """MNIST Dataset.
-
-    The train set contains 60000 images of handwritten digits. The data
-    contain 28*28=784 values in the range 0-255, the targets are numbers 0-9.
-    """
-    def __init__(self,
-                 name="mnist.train.npz",
-                 data_size=None,
-                 url="https://ufal.mff.cuni.cz/~courses/npfl129/2425/datasets/"):
+    def __init__(self, name="mnist.train.npz", data_size=None, url="https://ufal.mff.cuni.cz/~courses/npfl129/2425/datasets/"):
         if not os.path.exists(name):
             print("Downloading dataset {}...".format(name), file=sys.stderr)
             urllib.request.urlretrieve(url + name, filename="{}.tmp".format(name))
             os.rename("{}.tmp".format(name), name)
 
-        # Load the dataset, i.e., `data` and optionally `target`.
         dataset = np.load(name)
         for key, value in dataset.items():
             setattr(self, key, value[:data_size])
         self.data = self.data.reshape([-1, 28*28]).astype(float)
 
-
 def main(args: argparse.Namespace) -> Optional[npt.ArrayLike]:
     if args.predict is None:
-        # We are training a model.
+        # We are training a model
         np.random.seed(args.seed)
         train = Dataset()
 
-        # TODO: Train a model on the given dataset and store it in `model`.
-        model = ...
+        # Train the model (e.g., MLPClassifier)
+        model = MLPClassifier(hidden_layer_sizes=(128,), max_iter=20, random_state=args.seed)
+        model.fit(train.data, train.target)
 
-        # If you trained one or more MLPs, you can use the following code
-        # to compress it significantly (approximately 12 times). The snippet
-        # assumes the trained `MLPClassifier` is in the `mlp` variable.
-        #   mlp._optimizer = None
-        #   for i in range(len(mlp.coefs_)): mlp.coefs_[i] = mlp.coefs_[i].astype(np.float16)
-        #   for i in range(len(mlp.intercepts_)): mlp.intercepts_[i] = mlp.intercepts_[i].astype(np.float16)
-
-        # Serialize the model.
+        # Serialize the trained model with compression
         with lzma.open(args.model_path, "wb") as model_file:
             pickle.dump(model, model_file)
 
     else:
-        # Use the model and return test set predictions, either as a Python list or a NumPy array.
+        # We are predicting
         test = Dataset(args.predict)
 
+        # Load the model
         with lzma.open(args.model_path, "rb") as model_file:
             model = pickle.load(model_file)
 
-        # TODO: Generate `predictions` with the test set predictions.
-        predictions = ...
+        # Generate predictions
+        predictions = model.predict(test.data)
 
         return predictions
-
 
 if __name__ == "__main__":
     main_args = parser.parse_args([] if "__file__" not in globals() else None)
